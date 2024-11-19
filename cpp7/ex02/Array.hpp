4a221443c86b0d9260f0d779b3938a74382c7023
T0n7R <58948687+T0n7R@users.noreply.github.com>
2024-11-19 23:34:23 +0100

    cpp7 need debug last one catch

---
+++ cpp7/ex02/Array.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <iostream>
+#include <exception>
+#include <iomanip>
+
+#define GREEN "\033[32m"
+#define WHITE "\033[0m"
+
+template <typename T>
+class Array {
+
+    private:
+
+        unsigned int _size;
+        T *_array;
+
+    public:
+
+        Array(): _size(0), _array(new T[_size]) {};
+
+        Array(unsigned int size): _size(size), _array(new T[_size]) {
+            for (unsigned int i = 0; i < this->_size; i++)
+                this->_array[i] = T();
+        };
+
+        Array(Array const & src) : _size(src._size), _array(new T[_size]) {
+			for (unsigned int i = 0; i < this->_size; i++)
+				this->_array[i] = src._array[i];
+		}
+
+		~Array(void) {delete [] this->_array;}
+
+        T &operator[](unsigned int index) {
+            if (index >= this->_size)
+                throw (OutOfBoundException());
+            return this->_array[index];
+        }
+
+        const T& operator[](unsigned int index) const {
+            if (index >= this->_size)
+                throw (OutOfBoundException());
+            return this->_array[index];
+        }
+
+        Array &operator=(Array const &rhs) {
+			if (this != &rhs)
+			{
+				delete [] this->_array;
+				this->_size = rhs._size;
+				this->_array = new T[this->_size];
+				for (unsigned int i = 0; i < this->_size; i++)
+					this->_array[i] = rhs._array[i];
+			}
+			return (*this);
+        }
+
+        unsigned int size() const {
+            return (this->_size);
+        }
+
+        class OutOfBoundException : std::exception {
+            public:
+                virtual const char *what() const throw() {
+                    return ("Array is OOB\n");
+                }
+        };
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &out, const Array<T> &arr)
+{
+    out << GREEN "\n-------------------------------------" << "\n";
+    out << "|                TEST               |" << "\n";
+    out << "-------------------------------------" << WHITE << "\n";
+    out << "|";
+    for (unsigned int i = 0; i < arr.size(); ++i) {
+        out << std::setw(11) << arr[i] << "|";
+    }
+    out << "\n-------------------------------------\n\n";
+    return (out);
+}
---
+++ cpp7/ex02/main.cpp
@@ -0,0 +1,39 @@
+#include "Array.hpp"
+
+int main()
+{
+    try {
+        Array<std::string> str(3);
+        std::cout << "\nSize of Array is " << str.size() << '\n';
+        str[0] = "coucou";  
+        str[1] = "c est";  
+        str[2] = "moi";
+        std::cout << str[12] << '\n';
+    }
+    catch (std::exception &e) {
+        std::cout << e.what() << '\n';
+    }
+    try {
+
+        Array<int> str2(3);
+        std::cout << "\nSize of Array is " << str2.size() << '\n';
+        str2[0] = 1;  
+        str2[1] = 2;  
+        str2[2] = 3;
+        std::cout << str2 << "\n";
+    }
+    catch (std::exception &e) {
+        std::cout << e.what() << '\n';
+    }
+    try {
+        Array<float> str3(3);
+        std::cout << "\nSize of Array is " << str3.size() << '\n';
+        str3[0] = 1.1f;  
+        str3[1] = 2.1f;  
+        str3[2] = 3.1f;
+        std::cout << str3 << "\n";
+    }
+    catch (std::exception &e) {
+        std::cout << e.what() << '\n';
+    }
+}
